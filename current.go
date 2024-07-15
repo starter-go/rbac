07f576473c13dbe28c592b0d1b08a4680fbd866e
xukun <xukun17@sina.com>
2024-07-15 11:05:59 +0800

    为 CurrentUser 结构添加了几个便捷函数

--- current.go
+++ current.go
@@ -1,6 +1,10 @@
 package rbac
 
-import "github.com/starter-go/base/lang"
+import (
+	"time"
+
+	"github.com/starter-go/base/lang"
+)
 
 // CurrentUser 表示当前用户
 type CurrentUser struct {
@@ -12,4 +16,35 @@ type CurrentUser struct {
 	ExpiredAt lang.Time    `json:"expired_at"` // 过期时间
 
 	Properties map[string]string `json:"properties"`
+}
+
+// GetProperty ...
+func (inst *CurrentUser) GetProperty(name string) string {
+	table := inst.Properties
+	if table == nil {
+		return ""
+	}
+	return table[name]
+}
+
+// SetProperty ...
+func (inst *CurrentUser) SetProperty(name, value string) {
+	table := inst.Properties
+	if table == nil {
+		table = make(map[string]string)
+		inst.Properties = table
+	}
+	table[name] = value
+}
+
+// SetPeriod 以 (t0 + max_age) 的形式设置 StartedAt & ExpiredAt
+func (inst *CurrentUser) SetPeriod(from time.Time, maxAge time.Duration) {
+	const min = time.Millisecond
+	if maxAge < min {
+		maxAge = min
+	}
+	t0 := lang.NewTime(from)
+	t1 := t0.Add(maxAge)
+	inst.StartedAt = t0
+	inst.ExpiredAt = t1
 }

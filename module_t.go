2329a6295b194c777059ebd4222665b5d4bbf226
xukun <xukun17@sina.com>
2024-07-17 13:08:42 +0800

    up to v0.0.11

--- module_t.go
+++ module_t.go
@@ -2,6 +2,6 @@ package rbac
 
 const (
 	theModuleName    = "github.com/starter-go/rbac"
-	theModuleVersion = "v0.0.10"
-	theModuleRev     = 10
+	theModuleVersion = "v0.0.11"
+	theModuleRev     = 11
 )
